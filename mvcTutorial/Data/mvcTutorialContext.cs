d601d0e93d0790f53db7988b4d82771bbb42ea77
Rob Gardner <gardnerr@gmail.com>
2019-02-14 22:40:00 -0500

    Initial Commit

---
+++ mvcTutorial/Data/mvcTutorialContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcTutorial.Models
+{
+    public class mvcTutorialContext : DbContext
+    {
+        public mvcTutorialContext (DbContextOptions<mvcTutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<mvcTutorial.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ mvcTutorial/Models/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace mvcTutorial.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new mvcTutorialContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<mvcTutorialContext>>()))
+            {
+                    //look for any movies
+                if (context.Movie.Any())
+                {
+                    return; //DB has been seeded already
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Star Wars: A New Hope",
+                        ReleaseDate = DateTime.Parse("1977 - 5 - 25"),
+                        Genre = "Science Fiction",
+                        Price = 15.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Star Wars: The Empire Strikes Back",
+                        ReleaseDate = DateTime.Parse("1980 - 5 - 27"),
+                        Genre = "Science Fiction",
+                        Price = 19.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Star Wars: Return of the Jedi",
+                        ReleaseDate = DateTime.Parse("1983 - 5 - 25"),
+                        Genre = "Science Fiction",
+                        Price = 15.99M
+                    }
+                );
+            }
+        }
+    }
+}
